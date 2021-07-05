779cfcf29a9cd4d6384da519dcbb9ea70e47455b
dazzlingeffy <irusya9@mail.ru>
2021-07-05 20:41:01 +0300

    feat: secure field, alert

--- AuthScreen/AuthViewController.m
+++ AuthScreen/AuthViewController.m
@@ -12,7 +12,11 @@
 
 @property (strong, nonatomic) IBOutlet UITextField *loginField;
 @property (strong, nonatomic) IBOutlet UITextField *passField;
+@property (strong, nonatomic) IBOutlet UILabel *secureField;
+@property (strong, nonatomic) IBOutlet UIStackView *secureStack;
 @property (strong, nonatomic) IBOutlet UIButton *authBtn;
+@property (strong, nonatomic) IBOutlet UIView *secureView;
+@property (strong, nonatomic) IBOutlet UIAlertController *alert;
 
 @end
 
@@ -26,6 +30,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // alert
+    
+    _alert = [UIAlertController alertControllerWithTitle:@"Welcome" message:@"You are successfuly authorized!" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *alertAction = [UIAlertAction actionWithTitle:@"Refresh" style:UIAlertActionStyleDestructive handler:nil];
+    [_alert addAction:alertAction];
+    
     // Do any additional setup after loading the view.
 
     // RSSchool header
@@ -94,11 +104,10 @@
     _authBtn.layer.borderColor = [[UIColor colorNamed: @"littleBoyBlue"] CGColor];
     _authBtn.layer.borderWidth = 2;
     _authBtn.layer.cornerRadius = 5;
+    _authBtn.clipsToBounds = YES;
     
     // add image to btn
     
-//    UIImage *img = [UIImage imageNamed:@"person"];
-//    UIImageView *imgView = [[UIImageView alloc] initWithImage: img];
     [_authBtn setImage:[UIImage imageNamed:@"person"] forState:UIControlStateNormal];
     [_authBtn setImage:[UIImage imageNamed:@"personFill"] forState:UIControlStateHighlighted];
     _authBtn.imageView.contentMode = UIViewContentModeScaleAspectFit;
@@ -114,6 +123,97 @@
     
     [_authBtn addTarget:self action:@selector(authBtnTapped:) forControlEvents:UIControlEventTouchUpInside];
     
+    // secure vstack
+    _secureView = [[UIView alloc] initWithFrame:CGRectMake(0, btnMargin + 67, 236, 130)];
+    _secureStack = [[UIStackView alloc] initWithFrame:CGRectMake(0, 0, 236, 110)];
+    
+    _secureStack.axis = UILayoutConstraintAxisVertical;
+    _secureStack.distribution = UIStackViewDistributionEqualCentering;
+    _secureStack.alignment = UIStackViewAlignmentCenter;
+    _secureStack.spacing = 20;
+    [_secureView setCenter: CGPointMake(self.view.center.x, _secureView.center.y)];
+    
+    
+    // secure code field
+//    CGFloat secureMargin = btnMargin + 80;
+    _secureField = [[UILabel alloc] initWithFrame: CGRectMake(0, 0, 100, 40)];
+    _secureField.text = @"_";
+    _secureField.font = [UIFont systemFontOfSize:18 weight:UIFontWeightSemibold];
+    [_secureField setTextAlignment:NSTextAlignmentCenter];
+    [_secureField setCenter: CGPointMake(self.view.center.x, _secureField.center.y)];
+    [_secureField.heightAnchor constraintEqualToConstant:100].active = true;
+    [_secureField.widthAnchor constraintEqualToConstant:70].active = true;
+    
+    [_secureStack addArrangedSubview:_secureField];
+    
+    // secure code buttons
+
+    UIStackView *btnStack = [[UIStackView alloc] init];
+
+    btnStack.axis = UILayoutConstraintAxisHorizontal;
+    btnStack.distribution = UIStackViewDistributionEqualSpacing;
+    btnStack.alignment = UIStackViewAlignmentCenter;
+    btnStack.spacing = 20;
+
+    UIButton *btnOne = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnOne.frame = CGRectMake(0, 0, 50, 50);
+    btnOne.titleLabel.font = [UIFont systemFontOfSize:24 weight:UIFontWeightSemibold];
+    [btnOne setTitle:@"1" forState:UIControlStateNormal];
+    [btnOne setTitleColor: [UIColor colorNamed: @"littleBoyBlue"] forState:UIControlStateNormal];
+    [btnOne setBackgroundColor:[UIColor colorNamed:@"white"]];
+    [btnOne.heightAnchor constraintEqualToConstant:50].active = true;
+    [btnOne.widthAnchor constraintEqualToConstant:50].active = true;
+
+    btnOne.layer.borderColor = [[UIColor colorNamed: @"littleBoyBlue"] CGColor];
+    btnOne.layer.borderWidth = 1.5;
+    btnOne.layer.cornerRadius = 25;
+    btnOne.clipsToBounds = YES;
+    [btnOne setBackgroundColor:[[UIColor colorNamed:@"littleBoyBlue"] colorWithAlphaComponent:0.2] forState:UIControlStateHighlighted];
+    
+    UIButton *btnTwo = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnTwo.frame = CGRectMake(0, 0, 50, 50);
+    btnTwo.titleLabel.font = [UIFont systemFontOfSize:24 weight:UIFontWeightSemibold];
+    [btnTwo setTitle:@"2" forState:UIControlStateNormal];
+    [btnTwo setTitleColor: [UIColor colorNamed: @"littleBoyBlue"] forState:UIControlStateNormal];
+    [btnTwo setBackgroundColor:[UIColor colorNamed:@"white"]];
+    [btnTwo.heightAnchor constraintEqualToConstant:50].active = true;
+    [btnTwo.widthAnchor constraintEqualToConstant:50].active = true;
+    [btnTwo setBackgroundColor:[[UIColor colorNamed:@"littleBoyBlue"] colorWithAlphaComponent:0.2] forState:UIControlStateHighlighted];
+
+    btnTwo.layer.borderColor = [[UIColor colorNamed: @"littleBoyBlue"] CGColor];
+    btnTwo.layer.borderWidth = 1.5;
+    btnTwo.layer.cornerRadius = 25;
+    btnTwo.clipsToBounds = YES;
+    
+    UIButton *btnThree = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnThree.frame = CGRectMake(0, 0, 50, 50);
+    btnThree.titleLabel.font = [UIFont systemFontOfSize:24 weight:UIFontWeightSemibold];
+    [btnThree setTitle:@"3" forState:UIControlStateNormal];
+    [btnThree setTitleColor: [UIColor colorNamed: @"littleBoyBlue"] forState:UIControlStateNormal];
+    [btnThree setBackgroundColor:[UIColor colorNamed:@"white"]];
+    [btnThree.heightAnchor constraintEqualToConstant:50].active = true;
+    [btnThree.widthAnchor constraintEqualToConstant:50].active = true;
+    [btnThree setBackgroundColor:[[UIColor colorNamed:@"littleBoyBlue"] colorWithAlphaComponent:0.2] forState:UIControlStateHighlighted];
+
+    btnThree.layer.borderColor = [[UIColor colorNamed: @"littleBoyBlue"] CGColor];
+    btnThree.layer.borderWidth = 1.5;
+    btnThree.layer.cornerRadius = 25;
+    btnThree.clipsToBounds = YES;
+    
+    // add btns target
+    
+    [btnOne addTarget:self action:@selector(numBtnTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [btnTwo addTarget:self action:@selector(numBtnTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [btnThree addTarget:self action:@selector(numBtnTapped:) forControlEvents:UIControlEventTouchUpInside];
+
+    [btnStack addArrangedSubview:btnOne];
+    [btnStack addArrangedSubview:btnTwo];
+    [btnStack addArrangedSubview:btnThree];
+    [_secureStack addArrangedSubview:btnStack];
+//    _secureStack.translatesAutoresizingMaskIntoConstraints = false;
+    [_secureView addSubview:_secureStack];
+//    [self.view addSubview:_secureView];
+    
     [self hideWhenTappedAround];
 }
 
@@ -123,21 +223,47 @@
     
     if (![login  isEqual: @"username"]) {
         self.loginField.layer.borderColor = [[UIColor colorNamed: @"venetianRed"] CGColor];
+    } else {
+        self.loginField.layer.borderColor = [[UIColor colorNamed: @"turquoiseGreen"] CGColor];
     }
     if (![pass isEqual: @"password"]) {
         self.passField.layer.borderColor = [[UIColor colorNamed: @"venetianRed"] CGColor];
+    } else {
+        self.passField.layer.borderColor = [[UIColor colorNamed: @"turquoiseGreen"] CGColor];
     }
     if ([login isEqual: @"username"] && [pass isEqual: @"password"]) {
-        self.loginField.layer.borderColor = [[UIColor colorNamed: @"turquoiseGreen"] CGColor];
         self.loginField.enabled = NO;
         self.loginField.alpha = 0.5;
-        self.passField.layer.borderColor = [[UIColor colorNamed: @"turquoiseGreen"] CGColor];
         self.passField.enabled = NO;
         self.passField.alpha = 0.5;
         self.authBtn.enabled = NO;
         self.authBtn.alpha = 0.4;
+        [self.view addSubview: self.secureView];
     }
-    NSLog(@"tap");
+}
+
+-(void)numBtnTapped:(UIButton *)sender {
+    if ([self.secureField.text isEqual:@"_"]) {
+        _secureView.layer.borderColor = nil;
+        _secureView.layer.borderWidth = 0;
+        _secureView.layer.cornerRadius = 0;
+        self.secureField.text = sender.titleLabel.text;
+    } else if (![self.secureField.text isEqual:@"_"] && self.secureField.text.length < 5) {
+        self.secureField.text = [NSString stringWithFormat:@"%@ %@", self.secureField.text, sender.titleLabel.text];
+    }
+    if (self.secureField.text.length == 5) {
+        if ([self.secureField.text isEqual:@"1 3 2"]) {
+            _secureView.layer.borderColor = [[UIColor colorNamed:@"turquoiseGreen"] CGColor];
+            _secureView.layer.borderWidth = 3;
+            _secureView.layer.cornerRadius = 10;
+            [self presentViewController:self.alert animated:YES completion:nil];
+        } else {
+            _secureView.layer.borderColor = [[UIColor colorNamed:@"venetianRed"] CGColor];
+            _secureView.layer.borderWidth = 3;
+            _secureView.layer.cornerRadius = 10;
+            self.secureField.text = @"_";
+        }
+    }
 }
 
 -(void)textFieldDidBeginEditing: (UITextField *)textField
